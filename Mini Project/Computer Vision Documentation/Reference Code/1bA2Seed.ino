f397cabd380aa29d6957ab14bd56fcab9d4fef76
madeleineh2025 <159819396+madeleineh2025@users.noreply.github.com>
2024-02-24 19:50:04 -0700

    Update 1bA2Seed.ino

--- Mini Project/Computer Vision Documentation/Reference Code/1bA2Seed.ino
+++ Mini Project/Computer Vision Documentation/Reference Code/1bA2Seed.ino
@@ -1,38 +1,42 @@
+// Pi to Arduino Communication Code
+
+// Global variables to be used for I2C communication.
 #include <Wire.h>
 #define MY_ADDR 8
-// Global variables to be used for I2C communication
 volatile uint8_t offset = 0;
 volatile uint8_t instruction[32] = {0};
 volatile uint8_t msgLength = 0;
 char stringInput[32] = {};
 volatile uint8_t reply = 0;
 
+// Initializes Arduino.
 void setup() {
   Serial.begin(115200);
-  // We want to control the built-in LED (pin 13)
+  // We want to control the built-in LED (pin 13).
   pinMode(LED_BUILTIN, OUTPUT);
-  // Initialize I2C
+  // Initialize I2C.
   Wire.begin(MY_ADDR);
-  // Set callbacks for I2C interrupts
+  // Set callbacks for I2C interrupts.
   Wire.onReceive(receive);
   Wire.onRequest(request);
 }
+
+// Runs program on Arduino.
 void loop() {
-  // If there is data on the buffer, read it
+  // If there is data on the buffer, read it.
   if (msgLength > 0) {
     if (offset==1) {
       digitalWrite(LED_BUILTIN,instruction[0]);
     }
-    // printString();
-    //stringReceive();
     printReceived();
     
     msgLength = 0;
   }
 }
-// printReceived helps us see what data we are getting from the leader
+
+// printReceived helps us see what data we are getting from the leader.
 void printReceived() {
-  // Print on serial console
+  // Print on serial console.
   Serial.print("Offset received: ");
   Serial.println(offset);
   Serial.print("Message Length: ");
@@ -43,11 +47,11 @@ void printReceived() {
     stringInput[i] = instruction[i];
   }
   Serial.println("");
-  //stringInput = instruction;
   Serial.print("String: ");
   Serial.println(stringInput);
 }
-// function called when an I2C interrupt event happens
+
+// Function called when an I2C interrupt event happens
 void receive() {
   // Set the offset, this will always be the first byte.
   offset = Wire.read();
@@ -66,14 +70,6 @@ void request() {
   Wire.write(reply);
   reply = 0;
  }
-/*
-void stringReceive() {
-  // Set the offset, this will always be the first byte.
-  offset = Wire.read();
-  // If there is information after the offset, it is telling us more about the command.
-  while (Wire.available()) {
-    stringInput = Wire.read();
-    
   }
 }
 */
