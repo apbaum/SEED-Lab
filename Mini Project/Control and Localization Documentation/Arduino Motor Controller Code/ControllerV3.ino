998278541aa999b043c7f35730717e37244913a2
madeleineh2025 <159819396+madeleineh2025@users.noreply.github.com>
2024-02-25 19:38:44 -0700

    Update ControllerV3.ino

--- Mini Project/Control and Localization Documentation/Arduino Motor Controller Code/ControllerV3.ino
+++ Mini Project/Control and Localization Documentation/Arduino Motor Controller Code/ControllerV3.ino
@@ -1,6 +1,6 @@
 // Mini Project Controller V3 Final Implementation Without Computer Vision
 // ================
-// Author: Madeleine Houghton
+// Authors: Madeleine Houghton and Quinn Hejmanowski
 // Date: 2/19/2024
 // ================
 // The purpose of this code is to design a PI controller
