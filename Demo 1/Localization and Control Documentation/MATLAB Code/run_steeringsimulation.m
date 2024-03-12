da69581621dd59b9d9a69b43ab33de2c2c419227
madeleineh2025 <159819396+madeleineh2025@users.noreply.github.com>
2024-03-12 00:26:38 -0600

    Update run_steeringsimulation.m

--- Demo 1/Localization and Control Documentation/MATLAB Code/run_steeringsimulation.m
+++ Demo 1/Localization and Control Documentation/MATLAB Code/run_steeringsimulation.m
@@ -1,12 +1,13 @@
 % run_steeringsimulation
+% Adapted by: Madeleine Houghton
 %
-% runs a simulation of a two wheeled robot with a forward motion and anglual
+% Runs a simulation of a two wheeled robot with a forward motion and anglual
 % motion controller. This script sets up
 % the parameters of the robot, and the input voltages as timeseries
-% The simulink model 'steeringsimulation' is run, and the results plotted
+% The simulink model 'steeringsimulation' is run, and the results plotted.
 
 
-% conversions from angle to counts and back
+% Conversions from angle to counts and back
 rad_to_counts = 3200/(2*pi);
 counts_to_rad = 1/rad_to_counts;
 r_measured_ft = 0.26365; % measured wheel radius;
@@ -26,10 +27,11 @@ sigma_r=10.7;
 K_l=1.4;
 sigma_l=11.7;
 
+% Sets inputs to the controller.
 phi_d=timeseries([0 5*pi/2 5*pi/2 5*pi/2],[0 2.5 9 30]);
 rho_d=timeseries([0 0 3 3],[0 6 15 30]);
 
-
+% Plots the results
 out=sim('steeringsimulation.slx')
 figure(1)
 clf
