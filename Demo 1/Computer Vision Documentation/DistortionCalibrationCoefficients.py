dbe7cfc504acd64f80c40d01daa3dbbf32c7c9f7
madeleineh2025 <159819396+madeleineh2025@users.noreply.github.com>
2024-03-12 11:29:47 -0600

    Update DistortionCalibrationCoefficients.py

--- Demo 1/Computer Vision Documentation/DistortionCalibrationCoefficients.py
+++ Demo 1/Computer Vision Documentation/DistortionCalibrationCoefficients.py
@@ -10,10 +10,10 @@ import numpy as np
 import cv2
 
 
-# termination criteria
+# Termination criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+# Prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
 objp = np.zeros((6*9,3), np.float32)
 objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
  
