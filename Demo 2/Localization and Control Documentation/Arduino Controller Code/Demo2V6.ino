c054c5c3553a957133e1cca697c69a20ba108909
QHej <160056317+QHej@users.noreply.github.com>
2024-04-04 20:43:14 -0600

    Create Demo2V6.ino

---
+++ Demo 2/Localization and Control Documentation/Arduino Controller Code/Demo2V6.ino
@@ -0,0 +1,610 @@
+// Global variables to be used for I2C communication.
+#include <Wire.h>
+#define MY_ADDR 8
+volatile uint16_t offset = 0;
+double instruction[64] = {0};
+volatile uint16_t msgLength = 0;
+char stringInput[64] = {};
+volatile uint8_t reply = 0;
+bool detected = false;
+bool start = true;
+
+// Assigns timing variables for sampling the motor velocities.
+unsigned long desired_Ts_ms = 10;
+unsigned long last_time_ms;
+unsigned long start_time_ms;
+bool startMove = false;
+bool task2 = true;
+
+// These variables keep track of the actual speed on the motors.
+float actual_speed[2] = {0, 0};
+
+// Assigns variables to keep track of the previous positions on
+// the encoders.
+float prev_pos[2] = {0, 0};
+
+// These variables are read from the Raspberry Pi to tell the robot
+// its current position.
+float currentAngle = 0;
+float currentPos = 0;
+float startPos = 0;
+
+// This holds calculated values for the controller to aim towards
+// a desired velocity for the angle and position.
+float desiredAngleVel = 0;
+float desiredDistVel = 0;
+
+// These variables are the user input of the target distance
+// and velocity of the robot.
+float TARGET_DISTANCE;
+float TARGET_ANGLE_DEG;
+
+// These parameters are used for the desired distance and angle.
+// The variables change since the input is simulated as a ramp
+// function.
+float desiredDistance = 0;
+float desiredAngle = 0;
+float riseTimeDist = 10;
+float riseTimeAngle = 10;
+float distanceRise = 0;
+float angleRise = 0;
+
+// Sets the target speed of the motors.
+float integralAngleError = 0;
+float integralDistError = 0;
+
+// Sets the distance and angle error of the robot. Also
+// sets the robot velocity and rotational velocuty errors.
+float distanceError = 0;
+float angleError = 0;
+float angleVelError = 0;
+float distanceVelError = 0;
+
+// Keeps track of the position of both motors in radians.
+long pos_counts[2] = {0, 0};
+float actual_pos[2] = {0, 0};
+
+// Sets motor pins and defines them in an array. Also determines
+// the voltage signs, motor voltage, and motor channels. This is
+// done to monitor the direction and speed on each motor.
+#define NUM_MOTOR_PINS 5
+int MotorPinArray[NUM_MOTOR_PINS] = {4, 7, 8, 9, 10};
+int Motor1Chan[2] = {2, 5};
+int Motor2Chan[2] = {3, 6};
+int VoltageSign[2] = {7,8};
+int MotorVoltage[2] = {9,10};
+
+// Initializes counting variables on the respective encoders.
+long counts[2] = {0, 0};
+int counter;
+int detectCount = 0;
+int turn;
+
+// Defines the states and variables for a FSM. This model was
+// used to simplify how the code will run.
+int state;
+const int IDLE_STATE = 0;
+const int LOCATE_STATE = 1;
+const int LINE_UP_STATE = 2;
+const int MOVE_STATE = 3;
+const int TURN_90_STATE = 4;
+const int CIRCLE_STATE = 5;
+
+// Initializes global variables for debouncing in the ISRs.
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 100;
+
+// Rotation and forward movement variables
+float robotAngle = 0;
+float prevRobotAngle = 0;
+float robotDistance = 0;
+float prevRobotDistance = 0;
+float instantRho = 0;
+float instantPhi = 0;
+
+// Variables to store change in distance and angle of robot.
+float currentdl[2] = {0,0};
+float lastdl[2] = {0,0};
+
+// Width of the robot and radius of the wheel. Both are in feet.
+float width = 0.9479;
+float radius = 0.26365;
+
+// Converts encoder counts to feet.
+double encClicksToFeet = 2*PI*radius/3200;
+
+// Defines variables for average voltage, difference voltage, voltage
+// to store in the motors, and PWM to write to the motors.
+float Voltage[2] = {0, 0};
+unsigned int PWM[2] = {0, 0};
+float avgVolt = 0;
+float difVolt = 0;
+
+// Defines the Interrupt Service Routine for changes on channel A and 
+// determining the rotation on the encoder. This ISR is for motor 1.
+void countEncoder1() {
+  // This condition decreases the likelihood of the ISR activating 
+  // during a bounce in the encoder channel transitions.
+  if (micros() - lastDebounceTime > debounceDelay) {
+
+    // Counts up twice for a clockwise rotation on the encoder.
+    if (digitalRead(Motor1Chan[0]) == digitalRead(Motor1Chan[1])){
+      counts[0] += 2;
+    }
+    // Counts down twice for a counter-clockwise rotation on encoder.
+    else {
+      counts[0] -= 2;
+    }
+
+    // Updates the last time this ISR was called.
+    lastDebounceTime = micros();
+  }
+}
+
+// Defines the Interrupt Service Routine for changes on channel A and 
+// determining the rotation on the encoder. This ISR is for motor 2.
+void countEncoder2() {
+  // This condition decreases the likelihood of the ISR activating 
+  // during a bounce in the encoder channel transitions.
+  if (micros() - lastDebounceTime > debounceDelay) {
+
+    // Counts up twice for a clockwise rotation on the encoder.
+    if (digitalRead(Motor2Chan[0]) == digitalRead(Motor2Chan[1])){
+      counts[1] += 2;
+    }
+    // Counts down twice for a counter-clockwise rotation on encoder.
+    else {
+      counts[1] -= 2;
+    }
+
+    // Updates the last time the ISR was called.
+    lastDebounceTime = micros();
+  }
+}
+
+void setup() {
+  // Initializes motor pins on the motor drive shield.
+
+  Wire.begin(MY_ADDR);
+  Wire.onReceive(receive);
+  Wire.onRequest(request);
+
+  for (int i = 0; i < NUM_MOTOR_PINS; i++) {
+    pinMode(MotorPinArray[i], OUTPUT);
+  }
+
+  // Initializes the encoder pins on motors 1 and 2.
+  for (int j = 0; j < 2; j++) {
+    pinMode(Motor1Chan[j], INPUT_PULLUP);
+    pinMode(Motor2Chan[j], INPUT_PULLUP);
+  }
+
+  // Initializes ISRs for motors 1 and 2. The ISR will
+  // activate when a change on channel A occurs on their
+  // respective motor.
+  attachInterrupt(digitalPinToInterrupt(Motor1Chan[0]), countEncoder1, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(Motor2Chan[0]), countEncoder2, CHANGE);
+
+  // Enables set up pin for motor drive shield.
+  digitalWrite(4, HIGH);
+
+  // Sets up monitoring the data collected from the program.
+  Serial.begin(115200);
+
+  // Initializes the times for the program.
+  last_time_ms = millis();
+  start_time_ms = last_time_ms;
+  desiredAngle = 0;
+  desiredDistance = 0;
+  state = IDLE_STATE;
+  counter = 0;
+}
+
+void loop() {
+  // These variables are the gains for the angle controller.
+  float Kp_angle = 15;
+  float Ki_angle= 11;
+  float angleVelKp = 4.5;
+  float maxAngleVel = 4;
+
+  // These variables are the gains for the distance controller.
+  float Kp_distance = 18.5;
+  float Ki_distance = 6;
+  float distanceVelKp = 4;
+  float maxDistVel = 3;
+
+  // Defines max battery voltage
+  float Battery_Voltage = 7.8;
+
+  // IDLE STATE
+  // In this state, it moves straight to the next state after resetting variables.
+  if (state == IDLE_STATE) {
+    // Activates when task is ready.
+    if (start == true) {
+      state = LOCATE_STATE;
+      startMove = false;
+      counter = 0;
+      TARGET_DISTANCE = 0;
+      TARGET_ANGLE_DEG = 0;
+      desiredAngle = 0;
+      desiredDistance = 0;
+    }
+  }
+  
+  // LOCATE STATE
+  // Turns until a marker is detected. If a marker is detected, goes to the next state.
+  else if (state == LOCATE_STATE) {
+    // Sets turning amount until marker is detected.
+    TARGET_ANGLE_DEG = 45 + 45*turn;
+    TARGET_DISTANCE = robotDistance;
+    riseTimeAngle = 2;
+    angleRise = TARGET_ANGLE_DEG*PI/180/(riseTimeAngle * 1000) * desired_Ts_ms;
+
+    // Simulates a ramp input for the desired angle. The robot will keep spinning
+    // until a marker is detected.
+    desiredAngle = desiredAngle + angleRise;
+
+    // If there is data on the buffer from the PI, read it.
+    if (msgLength > 0) {
+      if (offset == 1) {
+        digitalWrite(LED_BUILTIN,instruction[0]);
+      }
+      printReceived();
+      msgLength = 0;
+
+      // Tells robot it detected marker.
+      detected = true;
+    }
+
+    // If the marker is detected, move to the next state.
+    if (detected == true) {
+      counter = 0;
+      detectCount = 0;
+
+      state = LINE_UP_STATE;
+
+      startMove = false;
+
+      robotDistance = 0;
+      prevRobotDistance = 0;
+      robotAngle = 0;
+      prevRobotAngle = 0;
+      desiredDistance = 0;
+      desiredAngle = 0;
+      TARGET_DISTANCE = 0;
+      TARGET_ANGLE_DEG = 0;
+    }
+
+    // Otherwise, continues turning. If the first turn is met, has
+    // the robot turn again.
+    else {
+      if (startMove == true) {
+        turn = turn + 1;
+        startMove = false;
+        counter = 0;
+      }
+    }
+  } 
+  
+  else if (state == LINE_UP_STATE) {
+    if (detectCount < 100){
+        detectCount++;
+    } 
+    else {
+      if (detectCount == 101) {
+        if (msgLength > 0) {
+          if (offset == 1) {
+            digitalWrite(LED_BUILTIN,instruction[0]);
+          }
+          printReceived();
+          msgLength = 0;
+
+          // Tells robot it detected marker.
+          detected = true;
+        }
+        desiredAngle = currentAngle; 
+      }
+
+      if (abs(robotAngle-desiredAngle) < .5){
+        state = MOVE_STATE;
+        robotAngle = 0;
+        desiredAngle = 0;
+      }
+    }
+  }
+
+  else if (state == MOVE_STATE) {
+    // Sets target target distance.
+      TARGET_DISTANCE = 7;
+
+      // Sets up ramp function rises for the distance.
+      riseTimeDist = 10;
+      distanceRise = TARGET_DISTANCE/(riseTimeDist * 1000) * desired_Ts_ms;
+
+      // Simulates a ramp input for the desired distance. Only
+      // activates when angle is set.
+        if (abs(desiredDistance) >= abs(TARGET_DISTANCE)) {
+          desiredDistance = TARGET_DISTANCE;
+        }
+        else {
+          desiredDistance = desiredDistance + distanceRise;
+        }
+          // If the camera reads that the robot is within 1 ft. Goes to
+      // next state if task 2. 
+    if (abs(TARGET_DISTANCE - robotDistance) <= 1) {
+      
+      // Resets the variables for current position and angle for the next state.
+      counter = 0;
+      desiredAngle = 0;
+      desiredDistance = 0;
+      startMove = false;
+      counts[0] = 0;
+      counts[1] = 0;
+      lastdl[0] = 0;
+      lastdl[1] = 0;
+      prevRobotDistance = 0;
+      prevRobotAngle = 0;
+      TARGET_DISTANCE = 0;
+      TARGET_ANGLE_DEG = 0;
+      integralAngleError = 0;
+      integralDistError = 0;
+      
+      // Goes to 90 degree turn if task 2.
+      if (task2 == true) {
+        state = TURN_90_STATE;
+      }
+
+    // Otherwise goes back to idle state.
+      else {
+      start = false;
+      state = IDLE_STATE;
+      }
+    }
+  }
+
+  // TURN 90 DEG STATE
+  // Once the robot is within 1 ft of the marker, turns the robot 90 deg
+  // clockwise to prepare for circle.
+  else if (state == TURN_90_STATE) {
+    // Sets angle parameters.
+    TARGET_ANGLE_DEG = -90;
+    TARGET_DISTANCE = 0;
+    riseTimeAngle = 1;
+    angleRise = TARGET_ANGLE_DEG*PI/180/(riseTimeAngle * 1000) * desired_Ts_ms;
+    
+    // Simulates a ramp input for the desired angle. 
+    if (abs(desiredAngle) >= abs(TARGET_ANGLE_DEG*PI/180)) {
+      desiredAngle = TARGET_ANGLE_DEG*PI/180;
+    }
+    // End of ramp function.
+    else {
+      desiredAngle = desiredAngle + angleRise;
+    }
+
+    // Once the robot turns 90 degrees clockwise,
+    // goes to the next state. 
+    if ((abs(desiredAngle - (TARGET_ANGLE_DEG*PI/180)) < 0.5) && (task2 == true)) {
+      desiredAngle = 0;
+      desiredDistance = 0;
+      state = CIRCLE_STATE;
+
+      // To perform circle correctly, reset location.
+      counts[0] = 0;
+      counts[1] = 0;
+      lastdl[0] = 0;
+      lastdl[1] = 0;
+      prevRobotDistance = 0;
+      prevRobotAngle = 0;
+    }
+  }
+
+  // CIRCLE STATE
+  // Makes the robot move in a circle at some radius.
+  else if (state == CIRCLE_STATE) {
+    float turnRadius = 1;
+    TARGET_DISTANCE = 2*PI*turnRadius;
+    TARGET_ANGLE_DEG = 360 + 40;
+
+    // Sets up ramp function rises for the distance and angle.
+    riseTimeDist = 6;
+    riseTimeAngle = 6;
+    distanceRise = TARGET_DISTANCE/(riseTimeDist * 1000) * desired_Ts_ms;
+    angleRise = TARGET_ANGLE_DEG*PI/180/(riseTimeAngle * 1000) * desired_Ts_ms;
+
+    // Simulates a ramp input for the desired angle. 
+    if (abs(desiredAngle) >= abs(TARGET_ANGLE_DEG*PI/180)) {
+      desiredAngle = TARGET_ANGLE_DEG*PI/180;
+    }
+    else {
+      desiredAngle = desiredAngle + angleRise;
+    }
+
+    // Simulates a ramp input for the desired distance.
+    if (abs(desiredDistance) >= abs(TARGET_DISTANCE)) {
+      desiredDistance = TARGET_DISTANCE;
+    }
+    else {
+      desiredDistance = desiredDistance + distanceRise;
+    }
+  }
+
+  // By default goes to idle state.
+  else {
+    state = IDLE_STATE;
+  }
+
+  // Converts the encoder counts to feet.
+  currentdl[0] = (double)myEnc(1) * encClicksToFeet;
+  currentdl[1] = (double)myEnc(2) * encClicksToFeet;
+
+  // Updates last time program ran.
+  last_time_ms = millis();
+  
+  // Calculates the robot distance and angle.
+  // Distance is in feet and angle is in radians.
+  robotDistance = prevRobotDistance + (currentdl[0] - lastdl[0] + currentdl[1] - lastdl[1])/2;
+  robotAngle = prevRobotAngle - (currentdl[0] - lastdl[0] - currentdl[1] + lastdl[1])/width;
+  
+  // Updates previous variables for distance and angle.
+  prevRobotDistance = robotDistance;
+  prevRobotAngle = robotAngle;
+
+  // Updates last changes in encoder.
+  lastdl[0] = currentdl[0];
+  lastdl[1] = currentdl[1];
+
+  // Calculates the current positions on the motors in radians.
+  for (int i = 0; i < 2; i++) {
+    pos_counts[i] = myEnc(i + 1);
+    actual_pos[i] = 2*PI*(float)pos_counts[i]/3200;
+
+    // Calculates the actual velocities in rad/sec on the motors.
+    actual_speed[i] = (actual_pos[i] - prev_pos[i])/((float)desired_Ts_ms/1000);
+    prev_pos[i] = actual_pos[i];
+  }
+
+  // Calculates instantaneous forward velocity and rotational velocity.
+  instantRho = radius*(actual_speed[1] + actual_speed[0])/2;
+  instantPhi = radius*(actual_speed[1] - actual_speed[0])/width; 
+
+  // Calculates the phi error (angle) and rho error (distance).
+  distanceError = desiredDistance - robotDistance;
+  angleError = desiredAngle - robotAngle;
+
+  // Calculates the desired angular velocity.
+  integralAngleError = integralAngleError + angleError*((float)desired_Ts_ms / 1000);
+  desiredAngleVel = Kp_angle * angleError + Ki_angle * integralAngleError;
+  angleVelError = desiredAngleVel - instantPhi;
+
+  // Calculates the desired position velocity.
+  integralDistError = integralDistError + distanceError*((float)desired_Ts_ms / 1000);
+  desiredDistVel = Kp_distance * distanceError + Ki_distance * integralDistError;
+
+  // Ensures the forward velocity is not above a large amount.
+  if (abs(desiredDistVel) >= maxDistVel) {
+    desiredDistVel = sgn(desiredDistVel) * maxDistVel;
+  }
+  distanceVelError = desiredDistVel - instantRho;
+
+  // Ensures the rotational velocity is not above a large amount.
+  if (abs(desiredAngleVel) >= maxAngleVel) {
+    desiredAngleVel = sgn(desiredAngleVel) * maxAngleVel;
+  } 
+  angleVelError = desiredAngleVel - instantPhi;
+
+  // Calculates the average and difference voltages.
+  difVolt = angleVelKp * angleVelError;
+  avgVolt = distanceVelKp * distanceVelError;
+
+
+// Sets the voltage on the motors based on the average and difference voltages.
+  // Inverted because motor 1 is on the left and motor 2 is on the right.
+  Voltage[0] = (avgVolt - difVolt)/2;   // Motor 1
+  Voltage[1] = (avgVolt + difVolt)/2;   // Motor 2
+/*
+  Serial.print(Voltage[0]);
+  Serial.print("\t");
+  Serial.println(Voltage[1]);*/
+
+  // Sets speed and direction on motors.
+  for (int k = 0; k < 2; k++) {
+    if (Voltage[k] > 0) {
+      // Rotates the motors forward.
+      digitalWrite(VoltageSign[k], HIGH);  
+    }
+    else {
+      // Rotates the motors backward.
+      digitalWrite(VoltageSign[k], LOW);  
+    }
+
+    // Saturates the voltage to be within the batter voltage range.
+    Voltage[k] = min(abs(Voltage[k]), Battery_Voltage);
+
+    // Applies the saturated requested voltage and models as PWM signal.
+    PWM[k] = 255 * Voltage[k] / Battery_Voltage;
+
+    // Sets motor speed
+    analogWrite(MotorVoltage[k], PWM[k]);
+  }
+
+  // Samples every desired sample rate.
+  while (millis() < last_time_ms + desired_Ts_ms) {
+    // Waits until desired time passes.
+  }
+}
+
+// ================
+// FUNCTIONS
+// ================
+
+// Adjusts the count on the channels to be more precise for both
+// motors 1 and 2. 
+long myEnc(int motor) {
+  // Activates to adjust the count on the motor 1 encoder.
+  if (motor == 1) {
+    if (digitalRead(Motor1Chan[0]) != digitalRead(Motor1Chan[1])) {
+      return (counts[0] + 1);
+    }
+    else {
+      return counts[0];
+    }
+  }
+
+  // Activates to adjust the count on the motor 2 encoder.
+  else if (motor == 2) {
+    if (digitalRead(Motor2Chan[0]) != digitalRead(Motor2Chan[1])) {
+      return (counts[1] + 1);
+    }
+    else {
+      return counts[1];
+    }  
+  }
+}
+
+// Sets what data will be received by Pi for data communication.
+void printReceived() {
+  for (int i = 0; i < msgLength; i++) {
+    stringInput[i] = instruction[i];
+    currentAngle = instruction[i];
+    if ((int)currentAngle & 0x80) {
+      currentAngle = -((~(int)currentAngle + 1) & 0x7F);
+    }
+    currentAngle = currentAngle/10;
+  }
+}
+
+// Requests data to fix timing.
+void request() {
+  // According to the Wire source code, we must call write() within the requesting ISR
+  // and nowhere else. Otherwise, the timing does not work out. See line 238:
+  // https://github.com/arduino/ArduinoCore-avr/blob/master/libraries/Wire/src/Wire.cpp
+  Wire.write(reply);
+  reply = 0;
+ }
+
+ // Returns a sign of the value.
+ int sgn(float value) {
+  if (value > 0) {
+    return 1;
+  }
+  else if (value < 0) {
+    return -1;
+  }
+  else {
+    return 0;
+  }
+ }
+
+ void receive() {
+  // Set the offset, this will always be the first byte.
+  offset = Wire.read();
+  // If there is information after the offset, it is telling us more about the command.
+  while (Wire.available()) {
+    instruction[msgLength] = Wire.read();
+    msgLength++;
+  }
+}
+
+// ================
+// CODE ENDS HERE
+// ================
